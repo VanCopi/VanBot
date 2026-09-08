16e3c3b526f6192fa3f8270d46d0dd2ad81e9348
Van <sidorenkov-ia@yandex.ru>
2026-09-08 21:10:39 +0300

    * Telegram bot was added

---
+++ src/main/java/com/github/javarushcommunity/vanbot_telegramBotJavaRush/config/BotConfig.java
@@ -0,0 +1,19 @@
+package com.github.javarushcommunity.vanbot_telegramBotJavaRush.config;
+
+import com.github.javarushcommunity.vanbot_telegramBotJavaRush.bot.JavaRushTelegramBot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class BotConfig {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(JavaRushTelegramBot bot) throws TelegramApiException {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(bot);
+        return api;
+    }
+}
